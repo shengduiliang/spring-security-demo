3fec842b087bfd66ecc920df303029bc57f28b6c
shengduiliang <1107722390@qq.com>
2024-12-31 20:30:23 +0800

    feat: 添加spring security入门项目

---
+++ spring-security-start/src/test/java/org/example/springsecuritystart/SpringSecurityStartApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springsecuritystart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityStartApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
